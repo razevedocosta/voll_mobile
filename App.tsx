586e6ca67dbeecdc9978d7b4342fba9d5d049466
Rodrigo Azevedo <razevedocosta@gmail.com>
2023-04-12 14:58:54 -0300

    criacao da tela de login

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import Login from "./src/Login";
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { TEMAS } from "./src/estilos/temas";
+
+export default function App() {
+  return (
+    <NativeBaseProvider theme={TEMAS}>
+      <StatusBar backgroundColor={TEMAS.colors.blue[800]} />
+      <Login />
+    </NativeBaseProvider>
+  );
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# voll_mobile
+
+Para executar o projeto
+npx expo start
+
+Passos
+- Criar o arquivo Login.tsx
+- Criar a pasta estilos
+- Criar a pasta assets
+- Criar a pasta @types
